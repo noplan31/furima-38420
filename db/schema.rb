2c98b8404665f98b59cedc083e013ace6e337a90
noplan31 <paparyu31@icloud.com>
2022-12-28 14:24:25 +0900

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 ActiveRecord::Schema.define(version: 2022_12_27_102059) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.date "birthday", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
